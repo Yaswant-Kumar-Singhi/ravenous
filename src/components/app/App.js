d1831cf11154137d6e36a1abf0b15ff5cb613c86
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-07-06 23:35:24 +0530

    creating bussiness component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../../src/logo.svg';
 import './App.css';
+import Business from '../Business/Business'
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      <Business />
     </div>
   );
 }
