db70524dc997bbc80a39329438d64f9496236e48
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-07-06 22:38:12 +0530

    Updates

---
+++ precache-manifest.72142c777c998cbd2167447a07c0e358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958cfce85117b40066c46b1037f05d4f",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "a41a1221e85b1ff51603",
+    "url": "/ravenous/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7a0757590d11a56e01e",
+    "url": "/ravenous/static/js/2.8f4389b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.8f4389b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41a1221e85b1ff51603",
+    "url": "/ravenous/static/js/main.b62a722a.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
